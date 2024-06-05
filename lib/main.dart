0407e71510445f0258615ae79a888d3563da00d2
Youri-program <youri6122002@gmail.com>
2024-06-05 23:07:23 +0200

    Update Homepage and Habit tiles
    Little update for the homepage and habit tiles for further improvement of the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'package:habit_tracker/pages/home_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  // initialize hive
+  await Hive.initFlutter();
+
+  // open a box
+  await Hive.openBox("Habit_Database");
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.green),
     );
   }
 }
